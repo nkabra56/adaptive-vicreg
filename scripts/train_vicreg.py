a8910a3f3651e963883d95421685a09777e05932
Nishant Kabra <nkabra56@gmail.com>
2025-11-23 00:46:16 -0600

    Squashed commit of the following:
    commit 3222757ebb750cbd5cf327c39c1c40cba9aabf77
    Author: Nishant Kabra <nkabra56@gmail.com>
    Date:   Sat Nov 22 21:06:36 2025 -0600
    
        updated with new metrics and minor vicreg train script changes
    
    commit aec42c718e31dde973cdd7ccee81bad7b5cff87e
    Author: Nishant Kabra <nkabra56@gmail.com>
    Date:   Tue Nov 18 17:54:07 2025 -0600
    
        ignoring some sh scripts for now will include once i run my own teststo make reproducability easier

--- scripts/train_vicreg.py
+++ scripts/train_vicreg.py
@@ -489,7 +489,7 @@ def main() -> None:
         trainer = VICRegTrainer(
             encoder=encoder,
             projector=projector,
-            w0=VICRegWeights(sim=25.0, var=25.0, cov=1.5),
+            w0=VICRegWeights(sim=25.0, var=25.0, cov=1.0),
             adaptive=args.adaptive,          # <── toggle: True => adaptive targets; False => baseline
             use_schedules=args.use_schedules,
             steps_per_epoch=steps_per_epoch,
